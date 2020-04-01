6b9a251b42d6410ca01e6d724863d39a4751235c
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-01 23:47:00 +0300

    created forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { DateCountPipe } from './date-count.pipe';
+// import { HighlightvoteDirective } from './highlightvote.directive';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
+// import { CountInputComponent } from './count-input/count-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
+    // HighlightvoteDirective,
+    QuoteFormComponent,
     DateCountPipe,
-    QuoteFormComponent
+    // CountInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
