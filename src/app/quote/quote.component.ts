e4d4f3e7b62006bfcce1d9e82d45c0f382c461b5
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-01 20:08:36 +0300

    added a pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,17 +9,22 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine'),
-    new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'LucycWigwa'),
-    new Quote(3, 'There is nothing good that comes easy', 'Denzel Ochieng'),
-    new Quote(3, 'Excuses are the nails that buikd the house of failer', 'Lovine Otieno')
+    new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine', new Date(2020,4,21)),
+    new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'LucycWigwa', new Date(2019,5,16)),
+    new Quote(3, 'There is nothing good that comes easy', 'Denzel Ochieng', new Date(2015,8,3),
+    // new Quote(4, 'A journey of a thousand miles begin with a single step', 'Lovine Otieno', new Date(2020,4,1)),
+    // new Quote(4, 'Excuses are the nails that build the house of failer', 'Lovine Otieno', new Date(2020,4,5)),
      ];
 
      toggleDetails(index){
        this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
      }
 
-     viewedQuote
+     viewedQuote(isViewed, index){
+       if (isViewed) {
+         this.quotes.splice(index,1);
+       }
+     }
 
   constructor() { }
 
