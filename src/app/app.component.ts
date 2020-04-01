299c46a5c4d2c895a4c60d72e5931b189d3ee954
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-01 10:06:31 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:string[];
+
+  constructor(){
+    this.quotes = ['Dont look at the clock, do what it does keep moving', 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'There is nothing good that comes easy']
+  }
 }
