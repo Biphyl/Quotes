8ac51bc9cb60a241a635ff1dd05fc81d2f01413d
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-01 11:30:23 +0300

    created class Quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quote[] = [
+    {id:1, name:'Dont look at the clock, do what it does keep moving'},
+    {id:2, name:'Difficulties in your life do not come to destroy you but to help you realize your potential and future'},
+    {id:3, name:'There is nothing good that comes easy'},
+    {id:3, name:'Excuses are the nails that buikd the house of failer'}
+     ];
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
