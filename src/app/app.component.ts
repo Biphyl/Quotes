c5a789e9f25354db8946d8e989b57a2c77fc538d
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-01 20:35:14 +0300

    adeed inbuilt date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,5 @@ export class AppComponent {
     new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine', new Date(2020,4,21)),
     new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'LucycWigwa', new Date(2019,5,16)),
     new Quote(3, 'There is nothing good that comes easy', 'Denzel Ochieng', new Date(2015,8,3),
-    // new Quote(3, 'Excuses are the nails that buikd the house of failer', 'Lovine Otieno', new Date(2020,4,4))
-     ];
+  ];
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,8 +12,6 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'Dont look at the clock, do what it does keep moving', 'Biron Lovine', new Date(2020,4,21)),
     new Quote(2, 'Difficulties in your life do not come to destroy you but to help you realize your potential and future', 'LucycWigwa', new Date(2019,5,16)),
     new Quote(3, 'There is nothing good that comes easy', 'Denzel Ochieng', new Date(2015,8,3),
-    // new Quote(4, 'A journey of a thousand miles begin with a single step', 'Lovine Otieno', new Date(2020,4,1)),
-    // new Quote(4, 'Excuses are the nails that build the house of failer', 'Lovine Otieno', new Date(2020,4,5)),
      ];
 
      toggleDetails(index){
