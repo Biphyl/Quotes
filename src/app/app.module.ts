c1d5396a40ec445319c19c1477fc0292ff971cbd
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-03 09:01:23 +0300

    update changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-// import { HighlightvoteDirective } from './highlightvote.directive';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { DateCountPipe } from './date-count.pipe';
 import { CountInputComponent } from './count-input/count-input.component';
-// import { CountInputComponent } from './count-input/count-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    // HighlightvoteDirective,
     QuoteFormComponent,
     DateCountPipe,
     CountInputComponent,
-    // CountInputComponent
+    CountInputComponent
   ],
   imports: [
     BrowserModule,
