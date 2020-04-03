3451bb08f1b9ad1d63ac6eeca523c769e7d23cbf
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-03 12:15:24 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,9 +0,0 @@
-export class Quote {
-  showDescription: boolean;
-  upvote: number;
-  downvote: number;
-  author: string;
-  constructor(public id: number,public name: string,public description:string, public postDate: Date){
-    this.showDescription=false;
-  }
-}
